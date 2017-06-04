b69d0556d7cd2893fd299815e8b7b1e0421fcccd
Tyler Lartonoix <larttyler@live.com>
2017-06-04 14:24:53 -0400

    Remove unused import

--- src/Responder.php
+++ src/Responder.php
@@ -4,7 +4,6 @@
 	use DaybreakStudios\Doze\Errors\AccessDeniedError;
 	use DaybreakStudios\Doze\Errors\ApiErrorInterface;
 	use DaybreakStudios\Doze\Errors\NotFoundError;
-	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
 	use Symfony\Component\Serializer\SerializerInterface;
 
